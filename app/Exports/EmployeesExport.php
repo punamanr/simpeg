8d239103ed0879758a0570f20d46f9f0998e58eb
unknown <purnama.n.rachman@gmail.com>
2019-03-13 14:18:58 +0700

    penambahan kolom alamat pada export excel data pegawai

--- app/Exports/EmployeesExport.php
+++ app/Exports/EmployeesExport.php
@@ -7,29 +7,58 @@ use App\Employee;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\FromQuery;
 use Maatwebsite\Excel\Concerns\Exportable;
+use Maatwebsite\Excel\Concerns\WithMapping;
+use Maatwebsite\Excel\Concerns\WithEvents;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 
-
-class EmployeesExport implements FromQuery, WithHeadings
+class EmployeesExport implements FromQuery,  WithHeadings, WithMapping//, WithEvents
 {
+    use Exportable;
+
+    // public function registerEvents(): array
+    // {
+
+    //   $styleArray = [
+    //     'font' => [
+    //       'bold' => true,
+    //     ]
+    //   ];
+
+    //   return [
+
+    //     BeforeSheet::class => function(BeforeSheet $event) use ($styleArray) {
+    //       $event->sheet->setCellValue('A1','Hello');
+    //     },
+
+    //     AfterSheet::class => function (AfterSheet $event) use ($styleArray) {
+    //       $event->sheet->getStyle('A1:E1')->applyFromArray($styleArray);
+    //     },
+    //   ];
+    // }
+
     /**
     * @return \Illuminate\Support\Collection
     */
     public function query()
     {
 
-      //return Employee::select('nip','nama_lengkap','status_pns');
+      return Employee::select('employees.nip','employees.nama_lengkap',DB::raw('CASE WHEN employees.status_pns = "1" THEN "PNS" ELSE "TTPK" END as status'),
+        'units.nama_unit','employees.formasi_jabatan','employees.alamat',DB::raw('CONCAT(" RT ",employees.rt) as rt'),DB::raw('CONCAT(" RW ",employees.rw) as rw'),
+        DB::raw('CONCAT(" Desa ",employees.kelurahan_desa) as kelurahan_desa'),DB::raw('CONCAT(" Kecamatan ",employees.kecamatan) as kecamatan'))
+      ->join('units', 'employees.kode_unit_kerja','=','units.id')
+      ->orderByRaw('nip ASC');
+    }
 
-      // $employee = Employee::select('nip','nama_lengkap','status_pns','formasi_jabatan')->get();
-      // return Excel::create('data_pegawai', function($excel) use ($employee){
-      //     $excel->sheet('mysheet', function($sheet) use ($employee){
-      //       $sheet->fromArray($employee);
-      //     });
-      // });
-
-
-      return Employee::select('employees.nip','employees.nama_lengkap',DB::raw('CASE WHEN employees.status_pns = "1" THEN "PNS" ELSE "TTPK" END'),'units.nama_unit','employees.formasi_jabatan')
-      ->join('units', 'employees.kode_unit_kerja','=','units.id');
+    public function map($employee): array
+    {
+      return [
+        $employee->nip,
+        $employee->nama_lengkap,
+        $employee->status,
+        $employee->nama_unit,
+        $employee->formasi_jabatan,
+        $employee->alamat.$employee->rt.$employee->rw.$employee->kelurahan_desa.$employee->kecamatan,
+      ];
     }
 
     public function headings(): array
@@ -40,7 +69,8 @@ class EmployeesExport implements FromQuery, WithHeadings
         'NAMA LENGKAP',
         'STATUS PEGAWAI',
         'UNIT KERJA',
-        'JABATAN'
+        'JABATAN',
+        'ALAMAT',
       ];
     }
 }
